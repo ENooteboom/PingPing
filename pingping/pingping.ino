8f0d6a1029c26f99991776e61452b785b89230a3
Eric Nooteboom <32373089+ENooteboom@users.noreply.github.com>
2020-09-04 11:06:23 -0700

    Update pingping.ino

--- pingping/pingping.ino
+++ pingping/pingping.ino
@@ -1,3 +1,15 @@
 //follow me @ https://www.twitch.tv/noot_gaming to watch me code this live
 
 //        UwU
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait
+}
